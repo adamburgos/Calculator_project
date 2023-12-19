ec0d86a92fe1f667b4970da71b74af8d8802fbe6
adamburgos <adamburgos@icloud.com>
2023-12-19 01:51:26 -0600

    created .json files. added testing folder to test enter key functionality among other things. Need to install jest to fulfill testing environment.

--- js/main.js
+++ js/main.js
@@ -15,37 +15,3 @@ function calculateResult() {
         document.getElementById('display').value = 'Error';
     }
 }
-
-// Test for the functionality of the handleKeyPress function
-describe('handleKeyPress', () => {
-  test('should call calculateResult when the enter key is pressed', () => {
-    const calculateResult = jest.fn();
-    const event = {
-      key: 'Enter',
-      preventDefault: jest.fn(),
-    };
-    handleKeyPress(event, calculateResult);
-    expect(calculateResult).toHaveBeenCalled();
-  });
-
-
-  test('should not call calculateResult when a different key is pressed', () => {
-    const calculateResult = jest.fn();
-    const event = {
-      key: 'a',
-      preventDefault: jest.fn(),
-    };
-    handleKeyPress(event, calculateResult);
-    expect(calculateResult).not.toHaveBeenCalled();
-  });
-
-  test('should call preventDefault when the enter key is pressed', () => {
-    const calculateResult = jest.fn();
-    const event = {
-      key: 'Enter',
-      preventDefault: jest.fn(),
-    };
-    handleKeyPress(event, calculateResult);
-    expect(event.preventDefault).toHaveBeenCalled();
-  });
-});
